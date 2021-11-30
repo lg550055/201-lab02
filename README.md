801e5e5840e5583e078ff1bf9344b1588de8ab8f
Polo <lg550055@hotmail.com>
2021-11-30 13:46:29 -0600

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # 201-lab02
-Basic html, css &amp; js About me
+
+## Basic html, css &amp; js About me
+
+This is a learning exercise of basic html, css and Js.  It's a brief autobiography that asks the user a few questions to make it interactive.
+
+### Author: Polo
---
+++ js/app.js
@@ -0,0 +1,25 @@
+function welcome() {
+  const user = prompt('Welcome! What is your name?');
+  // console.log(user);
+  alert('Your name is ' + user);
+  return user
+}
+
+const questions = ['Did I grow up in Mexico?', 'Did I studied Chemical Engineering', 'Have I worked as a consultant?', 'Have I mostly worked in finance', 'Am I an expert software engineer?'];
+
+const ans = ['y','y','y','y','n'];
+
+const answers = ['I grew up in Mexico!', 'I studied Chemical Engineering and later got an MBA', 'I first worked as a consultant', 'But mostly I have worked in finance', 'I would love to be an expert software engineer'];
+
+function qs() {
+  let q = '';
+  for (let i=0; i<5; i++) {
+    q = prompt(questions[i] + '(y/n)').toLowerCase();
+    if (q == ans[i]) {
+      alert ('Your answer is correct!');
+    } else {
+      alert ('Your answer is not correct');
+    };
+    document.write((i+1)+ '.  '+ answers[i] + "<br>");
+  };
+}
