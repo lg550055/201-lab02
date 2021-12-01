e5c6338cdcd3dcab9ebf4d466995fbad450d8878
Polo <lg550055@hotmail.com>
2021-12-01 12:22:14 -0600

    Adds top10 ol, modifies reset.css ul, ol

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ const questions = ['Did I grow up in Mexico?', 'Did I studied Chemical Engineeri
 
 const ans = ['y','y','y','y','n'];
 
-const answers = ['I grew up in Mexico!', 'I studied Chemical Engineering and later got an MBA', 'I first worked as a consultant', 'But mostly I have worked in finance', 'I would love to be an expert software engineer'];
+const answers = ['I grew up in Mexico!', 'I studied Chemical Engineering and later got an MBA', 'I first worked as a consultant', 'But mostly, I have worked in finance', 'I would love to be an expert software engineer'];
 
 function qs() {
   let q = '';
@@ -22,6 +22,13 @@ function qs() {
     } else {
       alert ('Your answer is not correct');
     };
-    document.write((i+1)+ '.  '+ answers[i] + "<br>");
+    document.write("<li>"+answers[i]+"</li>");
+  };
+}
+
+function topTen() {
+  let stuff = ['Growth mindset','Agile','JS','CSS','for loops','map()','filter()','reduce()','pair programming','Python'];
+  for (let i=0; i<10; i++) {
+    document.write("<li>"+stuff[i]+"</li>");
   };
 }
