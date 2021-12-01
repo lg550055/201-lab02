8c4a9f31f3ea965aac1fe9237db7b0b1de6db71b
Polo <lg550055@hotmail.com>
2021-12-01 16:30:07 -0600

    Meets req of Lab 03, removes functions, adds iterative questions

---
+++ js/welcome.js
@@ -0,0 +1,6 @@
+const user = prompt('Welcome! What is your name?');
+// console.log(user);
+alert('Your name is ' + user);
+document.write("<h3>"+`Welcome, ${user}!`+"</h3>");
+document.write("<hr>");
+document.write("<h2>Quick Bio Facts</h2>");
